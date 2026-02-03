3728571264b54b159f882de3950d66515fd33e0b
ce-atg <ce.atg.git@gmail.com>
2026-02-03 14:10:20 -0600

    Update fct_orders.sql

--- dbt/models/marts/fct_orders.sql
+++ dbt/models/marts/fct_orders.sql
@@ -1,1 +1,15 @@
+select
+    o.order_id,
+    o.order_date,
+    o.status,
+
+    -- foreign keys
+    o.customer_id,
+    o.product_id,
+
+    -- measures
+    o.quantity,
+    o.total_amount
+
+from {{ ref('stg_orders') }} o
 
